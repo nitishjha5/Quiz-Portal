7d19fe70c4d0fb628fbda87d6b09aa2515314f00
Nitish <06nitishkumar061998@gmail.com>
2020-06-23 01:55:45 +0530

    model completed

--- Quiz/account/models.py
+++ Quiz/account/models.py
@@ -1,12 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 class Batch(models.Model):
 	batch=models.CharField(max_length=100)
+	def __str__(self):
+		return self.batch
 
 class Student(models.Model):
-
-	name=models.CharField(max_length=100)
-	email=models.EmailField(max_length=254)
-	password = models.CharField(max_length=50)
-	batch=models.ForeignKey(Batch,on_delete=models.CASCADE)
+	 user = models.OneToOneField(User, on_delete=models.CASCADE)
+	 name=models.CharField(max_length=100)
+	 email=models.EmailField(max_length=254)
+	 batch=models.ForeignKey(Batch,on_delete=models.CASCADE)
+	 
 	
+
+
+class Attempt(models.Model):
+	user=models.ForeignKey(Student,on_delete=models.CASCADE)
+	quiz=models.ForeignKey('quiz.Quiz',on_delete=models.CASCADE)
+	question=models.ForeignKey('quiz.Questions',on_delete=models.CASCADE)
+	optioon=models.ForeignKey('quiz.Option',on_delete=models.CASCADE)
+
+class Perfomance(models.Model):
+	user=models.ForeignKey(Student,on_delete=models.CASCADE)
+	quiz=models.ForeignKey('quiz.Quiz',on_delete=models.CASCADE)
+	marks_obtained=models.DecimalField(max_digits=5, decimal_places=2)
+	rank=models.IntegerField()
 	# Create your models here.
--- Quiz/quiz/models.py
+++ Quiz/quiz/models.py
@@ -1,25 +1,26 @@
 from django.db import models
-import datetime
-
+from datetime import datetime 
+from django.utils import timezone   
 
 
 class Quiz(models.Model):
-
 	title=models.CharField(max_length=100)
-    #starttime=models.DateTimeField(auto_now=False,auto_now_add=False) 
-    #endtime=models.DateTimeField(auto_now=False,auto_now_add=False )
+	start_time=models.DateTimeField(default=timezone.now)
+	end_time=models.DateTimeField(default=timezone.now)
 	no_of_ques=models.IntegerField()
 	full_marks=models.DecimalField(max_digits=5, decimal_places=2)
-	user=models.ForeignKey('account.Student',on_delete=models.CASCADE)
-
+	batch=models.ForeignKey('account.Batch',on_delete=models.CASCADE)
 	def __str__(self):
 		return self.title
 
 class Questions(models.Model):
 	quiz=models.ForeignKey(Quiz,on_delete=models.CASCADE)
+	qno=models.IntegerField(unique=True)
 	text=models.TextField()
 	image=models.ImageField(blank=True)
 	mark=models.DecimalField(max_digits=5, decimal_places=2)
+	def __str__(self):
+		return self.text[:10]
 
 class Option(models.Model):
 	ques=models.ForeignKey(Questions,on_delete=models.CASCADE)
