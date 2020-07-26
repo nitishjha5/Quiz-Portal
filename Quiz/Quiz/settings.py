ddb8ad5664d3a182cbfb96ecaa6ef9853bb59f6b
Nitish <06nitishkumar061998@gmail.com>
2020-07-27 02:26:24 +0530

    frontend

--- Quiz/Quiz/settings.py
+++ Quiz/Quiz/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'account',
     'rest_framework',
     'myapi',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'Quiz.urls'
@@ -122,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_WHITELIST = (
+         'localhost:3000/'
+     )
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css';
+import customLayout from './containers/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <customLayout>
+      </customLayout>
+    </div>
+  );
+}
+
+export default App;
